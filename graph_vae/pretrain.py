ff99a8d5d04b5ed38740e11c742aab1ef524b9b7
liar <yyxxyyzero@163.com>
2026-02-07 23:41:30 +0800

    update utils: add MultiTaskLoss

--- graph_vae/pretrain.py
+++ graph_vae/pretrain.py
@@ -50,47 +50,6 @@ def get_cfg_defaults():
     return cfg
 
 
-# class Classifier(torch.nn.Module):
-#     def __init__(self, in_channels: int, out_channels: int, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         self.mlp = torch.nn.Sequential(
-#             torch.nn.Linear(in_channels, out_channels),
-#             torch.nn.LeakyReLU(),
-#             torch.nn.Linear(out_channels, out_channels),
-#             torch.nn.Dropout(p=0.3),
-#         )
-
-#     def forward(self, x):
-#         return self.mlp(x)
-
-
-# class MultiTaskLoss(nn.Module):
-#     def __init__(self, num_tasks=2):
-#         super(MultiTaskLoss, self).__init__()
-#         # 初始化 log_vars (对应公式中的 s)
-#         # 初始化为 0 相当于初始权重 a=1, b=1
-#         self.log_vars = nn.Parameter(torch.zeros(num_tasks))
-
-#     def forward(self, input_losses):
-#         """
-#         input_losses: 一个包含多个 loss 值的列表 [loss_rec, loss_reg]
-#         """
-#         # 确保输入 loss 和参数在同一个设备上
-#         total_loss = 0
-#         for i, loss in enumerate(input_losses):
-#             # 获取对应的 log_var
-#             log_var = self.log_vars[i]
-
-#             # 执行公式: 1/(2a^2) * loss + log(a)
-#             # 等价于: 0.5 * exp(-s) * loss + 0.5 * s
-#             weighted_loss = 0.5 * torch.exp(-log_var) * loss + 0.5 * log_var
-#             total_loss += weighted_loss
-
-#         return total_loss
-
-#     def par(self):
-#         return self.log_vars
-
 
 # @torch.compile (Debug时建议先注释掉，稳定后再开)
 def train_epoch(loader, optimizer, model, device):
@@ -108,25 +67,15 @@ def train_epoch(loader, optimizer, model, device):
 
         # Forward
         z, pred, target = model(data)
-        # y_hat = classifier(z)
-        # y_target = data.y.view(-1, 1).float()
-
-        # rec_loss = sce_loss(pred, target)
-        # reg_loss = reg_criterion(y_hat, y_target)
-
-        # loss = mtl_loss([rec_loss, reg_loss])
-        # loss = rec_loss + reg_loss
 
         loss = F.cross_entropy(pred, target)
 
         loss.backward()
         clip_grad_norm_(model.parameters(), max_norm=1.0)
         optimizer.step()
-        # total_rec_loss += rec_loss.item()
-        # total_reg_loss += reg_loss.item()
         total_loss += loss.item()
 
-    return total_loss  # , total_rec_loss, total_reg_loss
+    return total_loss
 
 
 def main(cfg):
---
+++ utils/multitask_loss.py
@@ -0,0 +1,30 @@
+import torch
+from torch import nn
+
+
+class MultiTaskLoss(nn.Module):
+    def __init__(self, num_tasks=2):
+        super(MultiTaskLoss, self).__init__()
+        # 初始化 log_vars (对应公式中的 s)
+        # 初始化为 0 相当于初始权重 a=1, b=1
+        self.log_vars = nn.Parameter(torch.zeros(num_tasks))
+
+    def forward(self, input_losses):
+        """
+        input_losses: 一个包含多个 loss 值的列表 [loss_rec, loss_reg]
+        """
+        # 确保输入 loss 和参数在同一个设备上
+        total_loss = 0
+        for i, loss in enumerate(input_losses):
+            # 获取对应的 log_var
+            log_var = self.log_vars[i]
+
+            # 执行公式: 1/(2a^2) * loss + log(a)
+            # 等价于: 0.5 * exp(-s) * loss + 0.5 * s
+            weighted_loss = 0.5 * torch.exp(-log_var) * loss + 0.5 * log_var
+            total_loss += weighted_loss
+
+        return total_loss
+
+    def par(self):
+        return self.log_vars
